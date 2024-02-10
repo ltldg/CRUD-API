811a7df34d2f67b60c8e319ec90c7b690cc7d2fe
ltldg <6061031@gmail.com>
2024-02-10 01:30:09 +0100

    feat:main task done add correct server in index

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
-
-
-
-
-
-
+import { createUser, getAllUsers, getUserByID, updateUser, deleteUser } from "./controller/controller";
+import * as http from 'http';
 
 
 
 
 const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url);
-    const parsedQuery = querystring.parse(parsedUrl.query);
+    const parsedUrl = req.url;
     const method = req.method;
 
-    if (method === 'POST' && parsedUrl.pathname === '/api/users') {
+    if (method === 'POST' && parsedUrl.includes('/api/users')) {
         createUser(req, res);
-    } else if (method === 'GET' && parsedUrl.pathname === '/api/users') {
+    } else if (method === 'GET' && parsedUrl.includes('/api/users')) {
         getAllUsers(req, res);
-    } else if (method === 'GET' && parsedUrl.pathname === '/api/users') {
+    } else if (method === 'GET' && parsedUrl.includes('/api/users')) {
         getUserByID(req, res);
-    } else if (method === 'PUT' && parsedUrl.pathname.startsWith('/api/users')) {
+    } else if (method === 'PUT' && parsedUrl.includes('/api/users')) {
         updateUser(req, res);
-    } else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/users')) {
+    } else if (method === 'DELETE' && parsedUrl.includes('/api/users')) {
         deleteUser(req, res);
     } else {
         res.statusCode = 404;
         res.end('Not found');
     }
 });
+
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}   )
